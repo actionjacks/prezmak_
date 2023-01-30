84b5ccd29901f9c41f7e4b3b81073b5dd5872073
dev-jack <actionjacks22@gmail.com>
2023-01-31 00:36:37 +0100

    2

--- src/components/ContactBox/ContactBox.tsx
+++ src/components/ContactBox/ContactBox.tsx
@@ -7,7 +7,7 @@ function ContactBox() {
     <div className="contact-wrapper">
       <AnimeBox duration={0.5}>
         <span className="phone-icon material-symbols-outlined">phone_in_talk</span>
-        <img className="flag-icon" src="./icons8-poland-100.png" alt="PL flag" />
+        <img className="flag-icon" src="../icons8-poland-100.png" alt="PL flag" />
         <div>
           <a className="phone" href="tel:+48787534890">
             +48 787 534 890
@@ -17,7 +17,7 @@ function ContactBox() {
 
       <AnimeBox duration={1}>
         <span className="phone-icon material-symbols-outlined">phone_in_talk</span>
-        <img className="flag-icon" src="./icons8-great-britain-100.png" alt="UK flag" />
+        <img className="flag-icon" src="../icons8-great-britain-100.png" alt="UK flag" />
         <div className="phone-contaner">
           <a className="phone" href="tel:+447783922730">
             +44 7783922730
