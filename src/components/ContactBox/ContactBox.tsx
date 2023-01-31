02df0ad46544ab3f3bb094a91e3f80e987d953da
dev-jack <actionjacks22@gmail.com>
2023-01-31 07:32:07 +0100

    1

--- src/components/ContactBox/ContactBox.tsx
+++ src/components/ContactBox/ContactBox.tsx
@@ -17,11 +17,7 @@ function ContactBox() {
 
       <AnimeBox duration={1}>
         <span className="phone-icon material-symbols-outlined">phone_in_talk</span>
-        <img
-          className="flag-icon"
-          src="../../icons8-great-britain-100.png"
-          alt="UK flag"
-        />
+        <img className="flag-icon" src="/icons8-great-britain-100.png" alt="UK flag" />
         <div className="phone-contaner">
           <a className="phone" href="tel:+447783922730">
             +44 7783922730
