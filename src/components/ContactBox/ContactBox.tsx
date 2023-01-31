cd1217c5a8f20d0f8c19a680acdcae327aa50407
dev-jack <actionjacks22@gmail.com>
2023-01-31 07:28:26 +0100

    2

--- src/components/ContactBox/ContactBox.tsx
+++ src/components/ContactBox/ContactBox.tsx
@@ -7,7 +7,7 @@ function ContactBox() {
     <div className="contact-wrapper">
       <AnimeBox duration={0.5}>
         <span className="phone-icon material-symbols-outlined">phone_in_talk</span>
-        <img className="flag-icon" src="../icons8-poland-100.png" alt="PL flag" />
+        <img className="flag-icon" src="icons8-poland-100.png" alt="PL flag" />
         <div>
           <a className="phone" href="tel:+48787534890">
             +48 787 534 890
